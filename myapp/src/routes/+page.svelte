374b15b104d21142f4d82a0ae26a53d2eb3a5c59
D4C <115446911+13D4C@users.noreply.github.com>
2025-01-30 22:18:28 +0700

    Initial commit

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+    import { goto } from '$app/navigation';
+    function RegisterPage() {
+    goto('/Register');
+  }
+</script>
+
+<main class="flex h-screen">
+    <div
+        class="flex-1 bg-cover bg-center"
+        style="background-image: url('https://scontent.fbkk6-2.fna.fbcdn.net/v/t39.30808-6/461275446_1082406396829060_2100932454648280842_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=8iH39aGh0owQ7kNvgE7LZym&_nc_oc=AdhPyTAyP7GBR4f71K9zanBIWbT1ILVJv1XmPOahTOy-awJme_X_HD0eKj8MpSfoAuw&_nc_zt=23&_nc_ht=scontent.fbkk6-2.fna&_nc_gid=AtFiFofr_aXyTNRmmsU0bLc&oh=00_AYBen4USCQgwE2wxET38tWG8jBF3WJTpQEB85tE9-X2ZKg&oe=67A10A7F');"
+    ></div>
+
+    <div class="flex items-center justify-center w-1/3 bg-white shadow-lg">
+        <div class="w-full max-w-md p-8 space-y-6">
+            <div class="text-center">
+                <img
+                    src="https://scontent.fbkk6-1.fna.fbcdn.net/v/t39.30808-6/461323765_1667997300443297_6234426937292606085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_ohc=JazFEVRFMFoQ7kNvgEA01rf&_nc_oc=AdizYuwiOTQO2O6B_kKA44ZbatjcXGnnWoH4XDFy6Ko2ld2mNx2SgWjdTqouWufV0Dg&_nc_zt=23&_nc_ht=scontent.fbkk6-1.fna&_nc_gid=AnAJiprmomPFB00J6l91eUA&oh=00_AYCUt5Top96sYU6L123eUy4dj7JxbYD8ooPpshptCuolwg&oe=67A129DB"
+                    alt="Logo"
+                    class="w-24 h-24 mx-auto mb-4"
+                />
+                <h1 class="text-2xl font-semibold text-gray-800">Login ดิ</h1>
+            </div>
+            <form class="space-y-4">
+                <div>
+                    <label
+                        for="username"
+                        class="block text-sm font-medium text-gray-700"
+                    >
+                        Username
+                    </label>
+                    <input
+                        type="text"
+                        id="username"
+                        class="block w-full px-3 py-2 mt-1 text-gray-700 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        placeholder="Enter your username"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        for="password"
+                        class="block text-sm font-medium text-gray-700"
+                    >
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        class="block w-full px-3 py-2 mt-1 text-gray-700 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        placeholder="Enter your password"
+                    />
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <a
+                        href="#"
+                        class="text-sm font-medium text-blue-600 hover:underline"
+                    >
+                        Forgot password?
+                    </a>
+
+                    <a
+                        href="#" on:click={RegisterPage}
+                        class="text-sm font-medium text-blue-600 hover:underline"
+                    >
+                        Register
+                    </a>
+                </div>
+
+                <div>
+                    <button
+                        type="submit"
+                        class="w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    >
+                        Sign In
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+
+<style>
+</style>
